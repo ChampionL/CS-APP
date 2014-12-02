c74527a8817b4c71efb02e121b1f5aa763607343
lizhengqian123 <li1zheng2qian3@gmail.com>
2014-12-02 22:58:11 +0800

    add code for pratice 2.8

--- ch2/float_and_int.c
+++ ch2/float_and_int.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define MASK 0x80
 typedef unsigned char * byte_point;
 
@@ -17,9 +20,12 @@ void show_binary(byte_point start, int n)
 }
 int main()
 {
-	int i = 12345;
+	int i = 3490593;
 	float f = i;
 	show_binary((byte_point)&i, sizeof(int));
 	show_binary((byte_point)&f, sizeof(float));
+
+	char *string = "ABCDEF";
+	show_binary((byte_point)string, strlen(string));
 	return 0;
 }
