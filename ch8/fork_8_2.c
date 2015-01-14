929bba65a63734fa5e213824a83d89014e3c1118
lzh2nix <li1zheng2qian3@gmail.com>
2015-01-15 05:46:21 +0800

    modify error output

--- ch8/fork_8_2.c
+++ ch8/fork_8_2.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
 
 int main()
 {
 	int i;
 	for ( i=0; i < 3; i++){
-		if (fork() < 0)
-			printf("ERROR");
+		if (fork() < 0){
+			fprintf(stderr, "ERROR:%d\n", strerror(errno));
+			exit(0);
+			}
 	}
 	printf("hello!!\n");
-	while(1);
 	return 0;
 }
