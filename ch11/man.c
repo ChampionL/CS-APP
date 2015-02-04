07cd40b7a89f4c7cc5e9080b071ec214604b6d53
lzh2nix <li1zheng2qian3@gmail.com>
2015-02-04 22:09:03 +0800

    add robust IO and test

---
+++ ch11/man.c
@@ -0,0 +1,18 @@
+#include "csapp.h"
+#define MAXLINE 10
+
+int main()
+{
+	int n;
+	rio_t rio;
+	
+	char buf[MAXLINE];
+		
+	rio_readinitb(&rio, STDIN_FILENO);
+
+	while((n = rio_readlineb(&rio, buf, MAXLINE)) != 0){
+		rio_writen(STDOUT_FILENO, buf, n);
+	}
+	
+	return 0;
+}
